ddfaf89d15a7f8df702715292d5c2fcc3540d24a
mtorres066 <mtorres066@gmail.com>
2012-10-09 10:52:04 -0600

    Se agrega un ejercicio para comparación de valores usando el "IF"

---
+++ EstructuraCondicionalCompuesta1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EstructuraCondicionalCompuesta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string linea;
+            Console.Write("Ingrese primer valor:");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            //SE REALIZA UNA COMPARACION DE LOS VALORES DE LAS VARIABLES
+            //CAPTURADAS DESDE CONSOLA.
+            if (num1 > num2)
+            {
+                //EN CASO DE QUE NUM1 SI SEA MAYOR A NUM2
+                Console.Write(num1);
+            }
+            else
+            {
+                Console.Write(num2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
