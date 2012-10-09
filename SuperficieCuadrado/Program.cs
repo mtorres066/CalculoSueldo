0c155b8c9ba578508a2abfa5855a342c02cfc546
mtorres066 <mtorres066@gmail.com>
2012-10-09 10:28:47 -0600

    Se agregó un proyecto mas a esta serie de practicas en consola

---
+++ SuperficieCuadrado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperficieCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado;
+            int superficie;
+            string linea;
+            Console.Write("Ingrese el valor del lado del cuadrado: ");
+            linea = Console.ReadLine();
+            lado = int.Parse(linea);
+            superficie = lado*lado;
+            Console.Write("La superficie del cuadrado es: ");
+            Console.Write(superficie);
+            Console.ReadKey();
+
+        }
+    }
+}
