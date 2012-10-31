f5e755ba93a8dd5e5f6d75ce22792b65c4673f56
mtorres066 <mtorres066@gmail.com>
2012-10-31 15:18:09 -0700

    Se agregaron dos proyectos para ver los metodos en que las clases regresan un valor

---
+++ EspacioMayorMenor/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Confeccionar una clase que permita ingresar tres valores por teclado. Luego mostrar el mayor y el menor
+namespace EspacioMayorMenor
+{
+    class MayorMenor
+    {
+
+        public void cargarValores()
+        {
+            string linea;
+            Console.Write("Ingrese primer valor:");
+            linea = Console.ReadLine();
+            int valor1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            int valor2 = int.Parse(linea);
+            Console.Write("Ingrese tercer valor:");
+            linea = Console.ReadLine();
+            int valor3 = int.Parse(linea);
+            int mayor, menor;
+            mayor = CalcularMayor(valor1, valor2, valor3);  //La llamada al método calcularMayor lo hacemos desde dentro del método CargarCalores
+            menor = CalcularMenor(valor1, valor2, valor3);
+            //Debemos asignar a una variable el valor devuelto por el método CalcularMayor. Luego el contenido de la variable mayor lo mostramos:
+            Console.WriteLine("El valor mayor de los tres es:" + mayor);
+            Console.WriteLine("El valor menor de los tres es:" + menor);
+        }
+        //Lo primero que podemos observar que el método retorna un entero y recibe tres parámetros:
+        //Dentro del método verificamos cual de los tres parámetros almacena un valor mayor, a este 
+        //valor lo almacenamos en una variable local llamada "m", al valor almacenado en esta variable 
+        //lo retornamos al final con un return.
+        public int CalcularMayor(int v1, int v2, int v3)
+        {
+            int m;
+            if (v1 > v2 && v1 > v3)
+            {
+                m = v1;
+            }
+            else
+            {
+                if (v2 > v3)
+                {
+                    m = v2;
+                }
+                else
+                {
+                    m = v3;
+                }
+            }
+            return m;
+        }
+
+        public int CalcularMenor(int v1, int v2, int v3)
+        {
+            int m;
+            if (v1 < v2 && v1 < v3)
+            {
+                m = v1;
+            }
+            else
+            {
+                if (v2 < v3)
+                {
+                    m = v2;
+                }
+                else
+                {
+                    m = v3;
+                }
+            }
+            return m;
+        }
+
+        static void Main(string[] args)
+        {
+            MayorMenor mm = new MayorMenor();
+            mm.cargarValores();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tabla/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Nombre de espacio principal
+namespace Tabla
+{
+    class TablaMultiplicar  // Nombre de la Clase
+    {
+        //El método CargarValores no tiene parámetros y tiene por objetivo cargar un valor 
+        //entero por teclado y llamar al método Calcular para que muestre la tabla de multiplicar 
+        //del valor que le pasamos por teclado:
+        public void CargarValor()
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.Write("Ingrese un valor (-1 para finalizar):");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor != -1)
+                {
+                    Calcular(valor);
+                    //Como vemos al método Calcular lo llamamos por su nombre y entre paréntesis 
+                    //le pasamos el dato a enviar (debe ser un valor o variable entera)
+                }
+            } while (valor != -1);
+        }
+        
+        //El método Calcular recibe un parámetro de tipo entero, luego lo utilizamos 
+        //dentro del método para mostrar la tabla de multiplicar de dicho valor, para 
+        //esto inicializamos la variable f con el valor que llega en el parámetro. Luego 
+        //de cada ejecución del for incrementamos el contador f con el valor de v.
+        public void Calcular(int v)
+        {
+            for (int f = v; f <= v * 10; f = f + v)
+            {
+                Console.Write(f + "-");
+            }
+            Console.WriteLine();
+        }
+        
+        //En este problema en la Main solo llamamos al método CargarValor, ya que el método 
+        //Calcular luego es llamado por el método CargarValor:
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tm = new TablaMultiplicar();
+            tm.CargarValor();
+        }
+    }
+}
