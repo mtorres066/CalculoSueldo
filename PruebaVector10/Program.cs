e77a11b64add1eccbd3ade1095691913c7fb3fce
mtorres066 <mtorres066@gmail.com>
2012-11-01 18:16:28 -0700

    Se agregaron 3 ejercicios mas Vectores

---
+++ PruebaVector10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* Si tenemos dos vectores de 5 elementos cada uno. En uno se almacenan los nombres de personas en el otro las edades de dichas personas.
+Decimos que el vector nombres es paralelo al vector edades si en la componente 0 de cada vector se almacena información relacionada a una persona (Juan - 12 años)
+Es decir hay una relación entre cada componente de los dos vectores.
+Esta relación la conoce únicamente el programador y se hace para facilitar el desarrollo de algoritmos que procesen los datos almacenados en las estructuras de datos.
+
+ * Desarrollar un programa que permita cargar 5 nombres de personas y sus edades respectivas. Luego de realizar la carga por teclado de todos los datos imprimir los nombres de las personas mayores de edad (mayores o iguales a 18 años) */
+
+namespace PruebaVector10
+{
+    class PruebaVector10
+    {
+        //Definimos los dos vectores:
+        private string[] nombres;
+        private int[] edades;
+
+        public void Cargar()
+        {
+            //Creamos los dos vectores con 5 elementos cada uno:
+            nombres = new string[5];
+            edades = new int[5];
+            //Mediante un for procedemos a la carga de los elementos de los vectores:
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("Ingrese nombre:");
+                nombres[f] = Console.ReadLine();
+                Console.Write("Ingrese edad:");
+                string linea;
+                linea = Console.ReadLine();
+                edades[f] = int.Parse(linea);
+            }
+        }
+        //Para imprimir los nombres de las personas mayores de edad verificamos cada componente del 
+        //vector de edades, en caso que sea igual o mayor o 18 procedemos a mostrar el elemento de la 
+        //misma posición del otro vector:
+        public void MayoresEdad()
+        {
+            Console.WriteLine("Personas mayores de edad.");
+            for (int f = 0; f < nombres.Length; f++) //Podemos utilizar la propiedad Length de cualquiera de los dos vectores, ya que tienen el mismo tamaño.
+            {
+                if (edades[f] >= 18)
+                {
+                    Console.WriteLine(nombres[f]);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector10 pv = new PruebaVector10();
+            pv.Cargar();
+            pv.MayoresEdad();
+        }
+    }
+}
---
+++ PruebaVector8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*Se desea almacenar los sueldos de operarios. 
+ * Cuando se ejecuta el programa se debe pedir la cantidad de sueldos a ingresar. 
+ * Luego crear un vector con dicho tamaño.*/
+
+namespace PruebaVector8
+{
+    class PruebaVector8
+    {
+        //La definición del vector no varía:
+        private int[] sueldos;
+
+        public void Cargar()
+        {
+            //Luego para la creación del mismo ingresamos una variable entera y la utilizamos 
+            //como subíndice en el momento de la creación del vector:
+            Console.Write("Cuantos sueldos cargará:");
+            string linea;
+            linea = Console.ReadLine();
+            int cant = int.Parse(linea);
+            sueldos = new int[cant];
+            for (int f = 0; f < sueldos.Length; f++)  //Luego las estructuras repetitivas las acotamos accediendo a la propiedad Length del vector:
+            {
+                Console.Write("Ingrese sueldo:");
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < sueldos.Length; f++)
+            {
+                Console.WriteLine(sueldos[f]);
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector8 pv = new PruebaVector8();
+            pv.Cargar();
+            pv.Imprimir();
+        }
+    }
+}
